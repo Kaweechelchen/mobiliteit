5cc23738debca55cccc36006719bc6de33a92308
Thierry Degeling <thierry.degeling@gmail.com>
2014-10-14 21:22:05 +0100

    whoops this is when you copy paste too much :D

--- app/mobiliteit/jsonControllerProvider.php
+++ app/mobiliteit/jsonControllerProvider.php
@@ -7,7 +7,7 @@
 
     class jsonControllerProvider implements ControllerProviderInterface {
 
-        static public function busses ( $app, $stationId, $limit ) {
+        static public function mobilityData ( $app, $stationId, $limit ) {
 
             $mobilityData = file_get_contents( 'http://travelplanner.mobiliteit.lu/hafas/cdt/stboard.exe/en?L=vs_stb&input=' . $stationId . '&boardType=dep&time=' . date( "H:i" ) . '&selectDate=today&start=yes&requestType=0&maxJourneys=' . $limit ) ;
 
@@ -15,7 +15,7 @@
                 substr( $mobilityData, 14 )
             );
 
-            return $app->json( $mobilityData );
+            return $mobilityData;
 
         }
 
@@ -25,13 +25,19 @@
 
             $ctr->get( '/', function( Application $app ) {
 
-                return 'you need to provide a twitter handle. Example: <a href="https://twjs.herokuapp.com/syn2cat">https://twjs.herokuapp.com/syn2cat</a>';
+                return 'you need to provide a satation id. Example: <a href="https://mobiliteit.herokuapp.com/200405036">https://mobiliteit.herokuapp.com/200405036</a>';
 
             });
 
             $ctr->get( '/{stationId}/{limit}', function( Application $app, $stationId, $limit ) {
 
-                return self::busses( $app, $stationId, $limit );
+                return $app->json(
+                    self::mobilityData(
+                        $app,
+                        $stationId,
+                        $limit
+                    )
+                );
 
             })->value('limit', 10);
 
