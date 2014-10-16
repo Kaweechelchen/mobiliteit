8c3d3cd795f0bafd8e839777fd31d554de320d1a
Thierry Degeling <thierry.degeling@gmail.com>
2014-10-16 12:05:30 +0100

    installed twig

--- app/app.php
+++ app/app.php
@@ -9,10 +9,18 @@
 
     $app[ 'debug' ] = true;
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/views',
+    ));
+
     $app->mount( '/api/1/', new mobiliteit\jsonControllerProvider() );
 
-    $app->get('/', function () use ( $app ) {
+    $app->get('/api', function () use ( $app ) {
         return $app->redirect('/api/1/');
+    });
+
+    $app->get('/', function () use ( $app ) {
+        return $app['twig']->render( 'getStation.html' );
     });
 
     $app->after(function (Request $request, Response $response) {
