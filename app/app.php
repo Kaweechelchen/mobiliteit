7fdbf9c579c66176335935408a674a7494f4eb2b
Thierry Degeling <thierry.degeling@gmail.com>
2014-10-16 12:06:32 +0100

    deactivated debugging mode

--- app/app.php
+++ app/app.php
@@ -7,8 +7,6 @@
 
     $app = new Silex\Application();
 
-    $app[ 'debug' ] = true;
-
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/views',
     ));
